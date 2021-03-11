e45613dd0f7373facf7c2dfd250296b7f099156b
Smitha TV <tvsmitha@gmail.com>
2021-03-11 19:40:30 +0530

    Add files via upload

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/smithatv1/go-hello-module
+
+go 1.15
+
+require rsc.io/quote v1.5.2
---
+++ hello.go
@@ -0,0 +1,9 @@
+package go-hello-module
+import (
+	"fmt"
+	"rsc.io/quote"
+)
+func Hello() string {
+	fmt.Println("This is hello from Hello in package go-hello-module");
+	return quote.Hello()
+}
